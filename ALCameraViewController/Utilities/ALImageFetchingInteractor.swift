fb8de39a1c08eacca415814fbef790a2c7117262
Justin Winter <justinlevi@gmail.com>
2015-12-18 15:10:49 -0500

    Update to allow fetching based on local identifiers
    This makes the class a bit more useful in other contexts

--- ALCameraViewController/Utilities/ALImageFetchingInteractor.swift
+++ ALCameraViewController/Utilities/ALImageFetchingInteractor.swift
@@ -40,23 +40,45 @@ public class ALImageFetchingInteractor {
         handleAuthorization(PHPhotoLibrary.authorizationStatus())
         return self
     }
+  
+    public func fetch(localIdentifiers: [String]) -> Self {
+      
+      // local identifiers can include slashes if they come from an asset
+      let lids = localIdentifiers.map { $0.componentsSeparatedByString("/").first ?? $0 }
+      
+      handleAuthorization(PHPhotoLibrary.authorizationStatus(), localIdentifiers: lids)
+      return self
+    }
     
-    private func onAuthorized() {
+    private func onAuthorized(localIdentifiers: [String]? = nil) {
         let options = PHFetchOptions()
         options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
         let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
         dispatch_async(dispatch_get_global_queue(priority, 0)) {
-            let assets = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: options)
+          let assets = (localIdentifiers != nil) ?
+            PHAsset.fetchAssetsWithLocalIdentifiers(localIdentifiers!, options: options) :
+            PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: options)
             var imageAssets = [PHAsset]()
             for asset in assets {
                 imageAssets.append(asset as! PHAsset)
             }
             dispatch_async(dispatch_get_main_queue()) {
+              if assets.count > 0 {
                 self.success?(assets: imageAssets)
+              }else{
+                self.noAssetsFetched()
+              }
             }
         }
     }
-    
+  
+    private func noAssetsFetched() {
+      let errorString = NSLocalizedString("error.zero-assets-found", tableName: StringsTableName, comment: "error.zero-assets-found")
+      let errorInfo = [NSLocalizedDescriptionKey: errorString]
+      let error = NSError(domain: errorDomain, code: 0, userInfo: errorInfo)
+      failure?(error: error)
+    }
+  
     private func onDeniedOrRestricted() {
         let errorString = NSLocalizedString("error.access-denied", tableName: StringsTableName, comment: "error.access-denied")
         let errorInfo = [NSLocalizedDescriptionKey: errorString]
@@ -64,22 +86,24 @@ public class ALImageFetchingInteractor {
         failure?(error: error)
     }
     
-    private func handleAuthorization(status: PHAuthorizationStatus) -> Void {
-        switch status {
-        case .NotDetermined:
-            if !authRequested {
-                PHPhotoLibrary.requestAuthorization(handleAuthorization)
-                authRequested = true
-            } else {
-                onDeniedOrRestricted()
-            }
-            break
-        case .Authorized:
-            onAuthorized()
-            break
-        case .Denied, .Restricted:
-            onDeniedOrRestricted()
-            break
+    private func handleAuthorization(status: PHAuthorizationStatus, localIdentifiers: [String]? = nil) -> Void {
+      switch status {
+      case .NotDetermined:
+        if !authRequested {
+          PHPhotoLibrary.requestAuthorization { status in
+            self.handleAuthorization(status, localIdentifiers: localIdentifiers)
+          }
+          authRequested = true
+        } else {
+          onDeniedOrRestricted()
         }
+        break
+      case .Authorized:
+        onAuthorized(localIdentifiers)
+        break
+      case .Denied, .Restricted:
+        onDeniedOrRestricted()
+        break
+      }
     }
 }
